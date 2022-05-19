57b6517f955dd25101ec5f9d1392c418cda2d8d6
Netzaalan <netzaalan@gmail.com>
2022-05-19 03:06:41 -0500

    Modificacion al CRUD de Materias

--- app/Views/Panel/materia_nueva.php
+++ app/Views/Panel/materia_nueva.php
@@ -14,7 +14,7 @@
             <div
               class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800"
             >
-            
+
             <?php
             $parametros = array('id' => 'formulario-materia-nueva'
                                 );
@@ -69,28 +69,30 @@
               <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Cuatrimestre:</span>
                 <?php
-                            $parametros = array('type' => 'text',
+                            $parametros = array('type' => 'number',
                                                 'class' => 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input',
                                                 'id' => 'cuatrimestre',
                                                 'name' => 'cuatrimestre',
-                                                'placeholder' => 'Cuatrimestre',
-                                                'value' => ''
+                                                'placeholder' => '1',
+                                                'value' => '1',
+                                                'min'=> '1',
+                                                'max'=> '9'
                                                 );
                             echo form_input($parametros);
                         ?>
               </label>
                                             </div>
                                             <div class="mt-4 text-sm">
-                                            
+
             <div class="text-center">
                 <a type="button" href="<?= route_to('dashboard') ?>" class="px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-md active:bg-purple-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-purple">Cancelar</a>
-                <button type="submit" class="px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" id="btn-guardar">Guardar usuario</button>
+                <button type="submit" class="px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" id="btn-guardar">Guardar materia</button>
             </div>
                                             </div>
         <?= form_close() ?>
 
                                             </div>
-            
+
 
 <?= $this->endSection() ?>
 
