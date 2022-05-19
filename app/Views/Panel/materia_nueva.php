78290f828ddf8c342dfa3f15cac172fde38687e8
feli20 <104938931+feli20@users.noreply.github.com>
2022-05-19 03:33:56 -0500

    C
    c

--- app/Views/Panel/materia_nueva.php
+++ app/Views/Panel/materia_nueva.php
@@ -76,16 +76,11 @@
                                                 'class' => 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input',
                                                 'id' => 'cuatrimestre',
                                                 'name' => 'cuatrimestre',
-<<<<<<< HEAD
-                                                'placeholder' => 'Cuatrimestre',
-                                                'value' => '',
-                                                'required' => true
-=======
                                                 'placeholder' => '1',
-                                                'value' => '1',
+                                                'value' => '',
                                                 'min'=> '1',
-                                                'max'=> '9'
->>>>>>> 2eeff1683586c4757342c8c5abd9405d74bf601c
+                                                'max'=> '9',
+                                                'required' => true
                                                 );
                             echo form_input($parametros);
                         ?>
